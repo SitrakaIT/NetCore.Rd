f86e05c6dc29e16e9a1b165774c7963a45fdea63
SitrakaIT <andriamparany.sitraka@gmail.com>
2019-03-28 19:04:27 +0000

    Core Library Service

---
+++ NetCore.Rd.Core/Generic/IGenericTransactCore.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections;
+using System.Threading.Tasks;
+
+namespace NetCore.Rd.Core.Generic
+{
+    public interface IGenericTransactCore<TEntity> where TEntity : class
+    {
+        Task<IEnumerable> QueryBySP(string name, params Tuple<string, string>[] paramCollections);
+    }
+}
